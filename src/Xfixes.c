bdebfcf8096b7403c5e59501d73e5b73d2e65f15
Jamey Sharp <jamey@minilop.net>
2010-04-09 17:26:31 -0700

    Always call SyncHandle after issuing an X request.

--- src/Xfixes.c
+++ src/Xfixes.c
@@ -91,6 +91,7 @@ XFixesExtAddDisplay (XFixesExtInfo *extinfo,
 	info->major_version = rep.majorVersion;
 	info->minor_version = rep.minorVersion;
 	UnlockDisplay (dpy);
+	SyncHandle ();
     } else {
 	/* The server doesn't have this extension.
 	 * Use a private Xlib-internal extension to hang the close_display
